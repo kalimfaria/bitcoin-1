51dee9c68ccc54c3145f3a93c5c28a9f3cf3ddf4
Faria Kalim <faria.kalim@gmail.com>
2018-04-11 12:34:46 -0500

    adding function to send transactions to all peers

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -468,7 +468,14 @@ UniValue createsignrawtransaction(const JSONRPCRequest& request) {
     std::cout << "Entered createsignrawtransaction" << std::endl;
     validateParams(request);
     UniValue hexString = createrawtransactionnochecks(request);
-    return signrawtransactionwithkeynochecks(request, hexString);
+    UniValue signedTransaction = signrawtransactionwithkeynochecks(request, hexString);
+
+    if (signedTransaction.isObject()) {
+        for (unsigned int i = 0; i < signedTransaction.keys.size(); i++)
+            if (signedTransaction.keys[i] == "hex")
+                return sendsignedrawtransaction(signedTransaction.values.at(i));
+
+    return signedTransaction;
 }
 
 
@@ -1193,6 +1200,80 @@ UniValue signrawtransaction(const JSONRPCRequest& request)
 #endif
     // If we have made it this far, then wallet is disabled and no private keys were given, so fail here.
     throw JSONRPCError(RPC_INVALID_PARAMETER, "No private keys available.");
+}
+
+UniValue sendsignedrawtransaction(string hex)
+{
+    ObserveSafeMode();
+
+    std::promise<void> promise;
+
+    // parse hex string from parameter
+    CMutableTransaction mtx;
+    if (!DecodeHexTx(mtx, hex))
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "TX decode failed");
+    CTransactionRef tx(MakeTransactionRef(std::move(mtx)));
+    const uint256& hashTx = tx->GetHash();
+
+    CAmount nMaxRawTxFee = maxTxFee;
+    if (!request.params[1].isNull() && request.params[1].get_bool())
+        nMaxRawTxFee = 0;
+
+    { // cs_main scope
+        LOCK(cs_main);
+        CCoinsViewCache &view = *pcoinsTip;
+        bool fHaveChain = false;
+        for (size_t o = 0; !fHaveChain && o < tx->vout.size(); o++) {
+            const Coin& existingCoin = view.AccessCoin(COutPoint(hashTx, o));
+            fHaveChain = !existingCoin.IsSpent();
+        }
+        bool fHaveMempool = mempool.exists(hashTx);
+        if (!fHaveMempool && !fHaveChain) {
+            // push to local node and sync with wallets
+            CValidationState state;
+            bool fMissingInputs;
+            if (!AcceptToMemoryPool(mempool, state, std::move(tx), &fMissingInputs,
+                                    nullptr /* plTxnReplaced */, false /* bypass_limits */, nMaxRawTxFee)) {
+                if (state.IsInvalid()) {
+                    throw JSONRPCError(RPC_TRANSACTION_REJECTED, FormatStateMessage(state));
+                } else {
+                    if (fMissingInputs) {
+                        throw JSONRPCError(RPC_TRANSACTION_ERROR, "Missing inputs");
+                    }
+                    throw JSONRPCError(RPC_TRANSACTION_ERROR, FormatStateMessage(state));
+                }
+            } else {
+                // If wallet is enabled, ensure that the wallet has been made aware
+                // of the new transaction prior to returning. This prevents a race
+                // where a user might call sendrawtransaction with a transaction
+                // to/from their wallet, immediately call some wallet RPC, and get
+                // a stale result because callbacks have not yet been processed.
+                CallFunctionInValidationInterfaceQueue([&promise] {
+                    promise.set_value();
+                });
+            }
+        } else if (fHaveChain) {
+            throw JSONRPCError(RPC_TRANSACTION_ALREADY_IN_CHAIN, "transaction already in block chain");
+        } else {
+            // Make sure we don't block forever if re-sending
+            // a transaction already in mempool.
+            promise.set_value();
+        }
+
+    } // cs_main
+
+    promise.get_future().wait();
+
+    if(!g_connman)
+        throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
+
+    CInv inv(MSG_TX, hashTx);
+    g_connman->ForEachNode([&inv](CNode* pnode)
+                           {
+                               pnode->PushInventory(inv);
+                           });
+
+    return hashTx.GetHex();
 }
 
 UniValue sendrawtransaction(const JSONRPCRequest& request)
