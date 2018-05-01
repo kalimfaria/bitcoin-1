90387843d4f112d4486fa84f003f7f6195ccf45c
Faria Kalim <faria.kalim@gmail.com>
2018-05-01 00:09:18 -0500

    logging cmpctblock

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2336,6 +2336,8 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
 
     else if (strCommand == NetMsgType::CMPCTBLOCK && !fImporting && !fReindex) // Ignore blocks received while importing
     {
+
+
         CBlockHeaderAndShortTxIDs cmpctblock;
         vRecv >> cmpctblock;
 
@@ -2350,7 +2352,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
                 connman->PushMessage(pfrom, msgMaker.Make(NetMsgType::GETHEADERS, chainActive.GetLocator(pindexBestHeader), uint256()));
             return true;
         }
-
+            LogPrintf("Verification: Msg type: CMPCTBLOCK %s", cmpctblock.header.GetHash());
         if (!LookupBlockIndex(cmpctblock.header.GetHash())) {
             received_new_header = true;
         }
